15ca27bcfcf2ea4b109fffde1b0fdefb7d95c88d
Marcin Drobik <mdrobik@future-processing.com>
2014-05-30 09:08:12 +0200

    Small refactor of PatternLimitedStream.

--- Mandro.Utils/PatternLimitedStream.cs
+++ Mandro.Utils/PatternLimitedStream.cs
@@ -7,40 +7,49 @@ using System.Text;
 namespace Mandro.Utils
 {
     /// <summary>
-    /// Represents stream that when pattern is found.
+    /// Stream decorator that ends when pattern is found.
     /// To continue reading call SearchAgain or SetPattern.
     /// </summary>
     public class PatternLimitedStream : Stream
     {
         private readonly Stream _baseStream;
         private byte[] _pattern;
-        private readonly List<byte> _searchBuffer;
-
+        private readonly List<byte> _searchBuffer = new List<byte>();
         private bool _finished;
 
         public PatternLimitedStream(Stream baseStream)
         {
             _baseStream = baseStream;
-            _searchBuffer = new List<byte>();
-            _pattern = null;
-            _finished = false;
         }
 
+        /// <summary>
+        /// Sets the pattern. Stream will end when pattern is found.
+        /// </summary>
+        /// <param name="pattern"></param>
         public void SetPattern(byte[] pattern)
         {
             _pattern = pattern;
             _finished = false;
         }
 
+        /// <summary>
+        /// Sets the pattern. It's assumed that pattern is UTF8 encoded.
+        /// Stream will end when pattern is found.
+        /// </summary>
+        /// <param name="pattern"></param>
         public void SetPattern(string pattern)
         {
             SetPattern(Encoding.UTF8.GetBytes(pattern));
         }
 
+        /// <summary>
+        /// Resets the stream so the pattern will be searched again.
+        /// </summary>
         public void SearchAgain()
         {
             _finished = false;
         }
+
 
         public override void Flush()
         {
@@ -59,26 +68,32 @@ namespace Mandro.Utils
 
         public override int Read(byte[] buffer, int offset, int requestedCount)
         {
-            var matchPattern = _pattern != null && _pattern.Length != 0;
-            if (!matchPattern && _searchBuffer.Count == 0)
+            // If pattern match is disabled but there's some leftover in search buffer
+            // then we can't read directly from underlaying stream.
+            var isPatternMatchEnabled = _pattern != null && _pattern.Length != 0;
+            if (!isPatternMatchEnabled && _searchBuffer.Count == 0)
             {
                 return _baseStream.Read(buffer, offset, requestedCount);
             }
 
-            if (matchPattern && _finished) return 0;
+            // If pattern was already found, do nothing until it's reset.
+            if (isPatternMatchEnabled && _finished) return 0;
 
-            var patternLength = matchPattern ? _pattern.Length : 0;
+            var patternLength = isPatternMatchEnabled ? _pattern.Length : 0;
 
-            var realRequestedCount = requestedCount + patternLength - _searchBuffer.Count;
-            var internalBuffer = new byte[realRequestedCount];
-            var bytesRead = _baseStream.Read(internalBuffer, 0, realRequestedCount);
+            
+            var internalRequestedCount = requestedCount + patternLength - _searchBuffer.Count;
+            var internalBuffer = new byte[internalRequestedCount];
+            var bytesRead = _baseStream.Read(internalBuffer, 0, internalRequestedCount);
             if (bytesRead == 0 && _searchBuffer.Count == 0) return 0;
 
             _searchBuffer.AddRange(internalBuffer.Take(bytesRead));
 
-            var find = matchPattern ? _searchBuffer.IndexOf(_pattern) : -1;
+            // Check if the pattern is present in search buffer. 
+            var find = isPatternMatchEnabled ? _searchBuffer.IndexOf(_pattern) : -1;
             if (find == -1)
             {
+                // Leave the patternLength bytes in search buffer
                 var bytesToCopy = Math.Min(_searchBuffer.Count - patternLength, requestedCount);
 
                 _searchBuffer.CopyTo(0, buffer, offset, bytesToCopy);
@@ -88,6 +103,7 @@ namespace Mandro.Utils
             }
             else
             {
+                // When pattern is found, remove it from search buffer
                 _finished = true;
                 _searchBuffer.CopyTo(0, buffer, offset, find);
                 _searchBuffer.RemoveRange(0, find + patternLength);
@@ -95,6 +111,9 @@ namespace Mandro.Utils
             }
         }
 
+        /// <summary>
+        /// Not implemented in Pattern Limited Stream.
+        /// </summary>
         public override void Write(byte[] buffer, int offset, int count)
         {
             throw new NotImplementedException();
