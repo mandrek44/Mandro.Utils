a089a18def0538b69bd8b2caaccdc4c87f2a13aa
Marcin Drobik <mdrobik@future-processing.com>
2014-07-06 22:08:18 +0200

    Added Installer related utils

---
+++ Mandro.Utils/Setup/ServiceDescription.cs
@@ -0,0 +1,9 @@
+﻿namespace Mandro.Utils.Setup
+{
+    public class ServiceDescription
+    {
+        public string ServiceName { get; set; }
+
+        public string Description { get; set; }
+    }
+}
