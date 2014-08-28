84477b8a9ba4f5ac6847e64601c943a98eacc06a
Marcin Drobik <mandrek44@gmail.com>
2014-08-28 21:51:56 +0200

    Added IAppBuilder diagnostic helpers

---
+++ Mandro.Utils.Owin/DiagnosticsHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using Owin;
+
+namespace Mandro.Utils.Owin
+{
+    public static class DiagnosticsHelper
+    {
+        /// <summary>
+        /// Logs all exceptions with given logger.
+        /// </summary>
+        public static void LogExceptions(this IAppBuilder appBuilder, Action<Exception> logger)
+        {
+            appBuilder.Use(
+                async (context, next) =>
+                {
+                    try
+                    {
+                        await next();
+                    }
+                    catch (Exception e)
+                    {
+                        logger(e);
+                        throw;
+                    }
+                });
+        }
+
+        /// <summary>
+        /// Logs all exceptions to stdout.
+        /// </summary>
+        public static void LogExceptions(this IAppBuilder appBuilder)
+        {
+            appBuilder.LogExceptions(e => Console.WriteLine(e.ToString()));
+        }
+
+        /// <summary>
+        /// Writes information about incoming calls to stdout.
+        /// </summary>
+        public static void TraceCalls(this IAppBuilder appBuilder)
+        {
+            appBuilder.Use(
+                async (context, next) =>
+                {
+                    Console.WriteLine("{0} {1} {2}", context.Request.Method, context.Request.Path, context.Request.QueryString);
+                    await next();
+                });
+        }
+    }
+}
